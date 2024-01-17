88170f2cbab44ed83693a2303fd1b6df2097f330
Joywin Bennis <107112207+joywin2003@users.noreply.github.com>
2024-01-17 22:26:50 +0530

    server sucessfuly run

--- src/index.js
+++ src/index.js
@@ -1,24 +1,26 @@
-import dotenv from "dotenv"
+import dotenv from "dotenv";
 import connectDB from "./db/index.js";
-
+import app from "./app.js";
 
 dotenv.config({
-    path: './.env'
-})
-
-
+  path: "./.env",
+});
 
 connectDB()
+  .then(() => {
+    app.listen(process.env.PORT || 3000, () => {
+      console.log(`App is listening on port ${process.env.PORT || 3000}`);
+    });
 
-
-
-
-
-
-
-
-
-
+    app.on("error", (error) => {
+      console.error("Unhandled error in the application:", error);
+      process.exit(1);
+    });
+  })
+  .catch((error) => {
+    console.error("Connection to the database failed:", error);
+    process.exit(1);
+  });
 
 /*
 import express from "express"
